946325c38d9de26d37016610702a001734a57b4e
lea6419 <le6736419@gmail.com>
2025-02-09 01:33:18 +0200

    Update service.js

--- ToDoListReact-master/src/service.js
+++ ToDoListReact-master/src/service.js
@@ -1,26 +1,46 @@
 import axios from 'axios';
 
-const apiUrl = "https://localhost:7271"
+const apiUrl = "process.env.REACT_APP_Api";
+axios.defaults.baseURL = apiUrl;
+
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    console.error("שגיאה ב-response:", error.response ? error.response.data : "Unknown error");
+    return Promise.reject(error);
+  }
+);
 
 export default {
   getTasks: async () => {
-    const result = await axios.get(`${apiUrl}/items`)    
+    const result = await axios.get('/');
     return result.data;
   },
 
-  addTask: async(name)=>{
-    console.log('addTask', name)
-    //TODO
-    return {};
+  addTask: async (taskName) => {
+    const response = await axios.post('/', {
+      name: taskName,
+      isComplete: false,
+    });
+    return response.data;
   },
 
-  setCompleted: async(id, isComplete)=>{
-    console.log('setCompleted', {id, isComplete})
-    //TODO
-    return {};
+
+  setCompleted: async (  todo, isComplete) => {
+    try {
+   
+      const result = await axios.put(`/${todo.id}?isDone=${isComplete}`, {
+            id: todo.id,
+            name: todo.name
+        });
+      return { result };
+    } catch (error) {
+      console.error("שגיאה בעידכון משימה:", error);
+      return { error: error.response ? error.response.data : "Unknown error" };
+    }
   },
 
-  deleteTask:async()=>{
-    console.log('deleteTask')
+  deleteTask: async (id) => {
+    await axios.delete(`/${id}`);
   }
 };
